8be8d5784dfb50dad66bb8699f22dd913a1c5e7f
DBoylan <47145188+DBoylan1@users.noreply.github.com>
2019-10-09 16:10:22 +0100

    Addition of directed acyclic grpahs
    Added basic code in java to make a directed acyclic graph.

--- LCA.java
+++ LCA.java
@@ -1,82 +1,129 @@
 
-import java.util.ArrayList; 
-import java.util.List; 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.LinkedList;
+import java.io.*;
+import java.util.*;
+
+//Java program to find single source shortest paths in Directed Acyclic Graphs 
+import java.io.*;
+import java.util.*;
+
+class directedAcyclic {
+	static final int INF = Integer.MAX_VALUE;
+
+	class AdjListNode {
+		private int v;
+		private int weight;
+
+		AdjListNode(int _v, int _w) {
+			v = _v;
+			weight = _w;
+		}
+
+		int getV() {
+			return v;
+		}
+
+		int getWeight() {
+			return weight;
+		}
+	}
+
+	class Graph {
+		private int V;
+		private LinkedList<AdjListNode> adj[];
+
+		Graph(int v) {
+			V = v;
+			adj = new LinkedList[V];
+			for (int i = 0; i < v; ++i)
+				adj[i] = new LinkedList<AdjListNode>();
+		}
+
+		void addEdge(int u, int v, int weight) {
+			AdjListNode node = new AdjListNode(v, weight);
+			adj[u].add(node);// Add v to u's list
+		}
+		 Graph newGraph(int number) 
+		    { 
+		        return new Graph(number); 
+		    } 
+
+	}
+}
 
 // A Binary Tree node 
-class Node { 
-	int data; 
-	Node left, right; 
+class Node {
+	int data;
+	Node left, right;
 
-	Node(int value) { 
-		data = value; 
-		left = right = null; 
-	} 
-} 
+	Node(int value) {
+		data = value;
+		left = right = null;
+	}
+}
 
-public class LCA 
-{ 
+public class LCA {
 
-	Node root; 
-	private List<Integer> path1 = new ArrayList<>(); 
-	private List<Integer> path2 = new ArrayList<>(); 
+	Node root;
+	private List<Integer> path1 = new ArrayList<>();
+	private List<Integer> path2 = new ArrayList<>();
 
-	// Finds the path from root node to given root of the tree. 
-	int findLCA(int n1, int n2) { 
-		path1.clear(); 
-		path2.clear(); 
-		return findLCAInternal(root, n1, n2); 
-	} 
+	// Finds the path from root node to given root of the tree.
+	int findLCA(int n1, int n2) {
+		path1.clear();
+		path2.clear();
+		return findLCAInternal(root, n1, n2);
+	}
 
-	private int findLCAInternal(Node root, int n1, int n2) { 
+	private int findLCAInternal(Node root, int n1, int n2) {
 
-		if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) { 
-			System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing"); 
-			System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing"); 
-			return -1; 
-		} 
+		if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) {
+			System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing");
+			System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing");
+			return -1;
+		}
 
-		int i; 
-		for (i = 0; i < path1.size() && i < path2.size(); i++) { 
-			 
-			if (!path1.get(i).equals(path2.get(i))) 
-				break; 
-		} 
+		int i;
+		for (i = 0; i < path1.size() && i < path2.size(); i++) {
 
-		return path1.get(i-1); 
-	} 
-	
-	// Finds the path from root node to given root of the tree, Stores the 
-	// path in a vector path[], returns true if path exists otherwise false 
-	public boolean findPath(Node root, int n, List<Integer> path) 
-	{ 
-		// base case 
-		if (root == null) { 
-			return false; 
-		} 
-		
-		// Store this node . The node will be removed if 
-		// not in path from root to n. 
-		path.add(root.data); 
+			if (!path1.get(i).equals(path2.get(i)))
+				break;
+		}
 
-		if (root.data == n) { 
-			return true; 
-		} 
+		return path1.get(i - 1);
+	}
 
-		if (root.left != null && findPath(root.left, n, path)) { 
-			return true; 
-		} 
+	// Finds the path from root node to given root of the tree, Stores the
+	// path in a vector path[], returns true if path exists otherwise false
+	public boolean findPath(Node root, int n, List<Integer> path) {
+		// base case
+		if (root == null) {
+			return false;
+		}
 
-		if (root.right != null && findPath(root.right, n, path)) { 
-			return true; 
-		} 
+		// Store this node . The node will be removed if
+		// not in path from root to n.
+		path.add(root.data);
 
-		// If not present in subtree rooted with root, remove root from 
-		// path[] and return false 
-		path.remove(path.size()-1); 
+		if (root.data == n) {
+			return true;
+		}
 
-		return false; 
-	} 
+		if (root.left != null && findPath(root.left, n, path)) {
+			return true;
+		}
 
-} 
- 
+		if (root.right != null && findPath(root.right, n, path)) {
+			return true;
+		}
 
+		// If not present in subtree rooted with root, remove root from
+		// path[] and return false
+		path.remove(path.size() - 1);
+
+		return false;
+	}
+
+}
