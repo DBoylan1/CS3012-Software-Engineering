192ca86706ef4df35dd6992acc05f6e0a9e76c41
DBoylan <47145188+DBoylan1@users.noreply.github.com>
2019-10-13 22:51:49 +0100

    Tests for edges and vertexes

--- LCAtest1.java
+++ LCAtest1.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 
 public class LCAtest1 {
 
-	//Tests for binary search tree
+	// Tests for binary search tree
 	@Test
 	public void testFindLCAInt() {
-		//Make binary tree
+		// Make binary tree
 		LCA tree = new LCA();
 		tree.root = new Node(1);
 		tree.root.left = new Node(2);
@@ -17,32 +17,32 @@ public class LCAtest1 {
 		tree.root.left.right = new Node(5);
 		tree.root.right.left = new Node(6);
 		tree.root.right.right = new Node(7);
-		
-		//Test for correct inputs
+
+		// Test for correct inputs
 		assertEquals("Checking for lowest common ancestor of nodes 4 and 5 ", 2, tree.findLCA(4, 5));
-		assertEquals("Checking for lowest common ancestor of nodes 2 and 4 ",2, tree.findLCA(2,4));
-	
-		//Test for incorrect input
+		assertEquals("Checking for lowest common ancestor of nodes 2 and 4 ", 2, tree.findLCA(2, 4));
+
+		// Test for incorrect input
 		LCA tree2 = new LCA();
 		tree2.root = null;
 		assertEquals("Checking for return of -1 for root 0 value", -1, tree2.findLCA(1, 2));
-		
-		//Test for root of n1 null
+
+		// Test for root of n1 null
 		LCA tree4 = new LCA();
 		tree4.root = new Node(1);
 		tree4.root.left = null;
 		tree4.root.right = new Node(2);
 		tree4.root.right.right = new Node(4);
 		assertEquals("Checking for return of -1 for null value", -1, tree4.findLCA(3, 4));
-		
-		//Test for root of n2 null
+
+		// Test for root of n2 null
 		LCA tree3 = new LCA();
 		tree3.root = new Node(1);
 		tree3.root.left = new Node(2);
 		tree3.root.right = null;
 		tree3.root.left.left = new Node(4);
 		assertEquals("Checking for return of -1 for null value", -1, tree3.findLCA(4, 5));
-		
+
 	}
 
 	@Test
@@ -51,26 +51,38 @@ public class LCAtest1 {
 		LCA tree = new LCA();
 		tree.root = null;
 		assertEquals("Checking for return of false for null root", false, tree.findPath(tree.root, 2, path1));
-		
+
 		ArrayList<Integer> path2 = new ArrayList();
 		LCA tree2 = new LCA();
 		tree2.root = new Node(1);
 		tree2.root.left = new Node(2);
 		tree2.root.right = new Node(3);
 		assertEquals("Checking for return of true for non null root", true, tree.findPath(tree2.root, 3, path2));
-		
-		
+
 	}
-	
-	//Tests for directed acyclic graphs
-	
-	//Test vertex
-	
-	//Test addEdge
-	
-	//Test indegree: Returns number of directed edges to vertex v 
-	
-	//Test outdegree Returns number of directed edges from vertex v
-	
-	//Test for cyclce
+
+	// Tests for directed acyclic graphs
+
+	@Test
+	public void testVertex() {
+		// Test of vertex
+		DAG test = new DAG(1);
+		assertEquals("testing V() function to return vertex", 1, test.V());
+	}
+
+	// Test addEdge
+	@Test
+	public void testEdge() {
+		// Test for edge
+		DAG test1 = new DAG(3);
+		assertEquals("testing with no edges", 0, test1.E());
+		test1.addEdge(1, 2);
+		assertEquals("testing with no edges", 1, test1.E());
+	}
+
+	// Test indegree: Returns number of directed edges to vertex v
+
+	// Test outdegree Returns number of directed edges from vertex v
+
+	// Test for cyclce
 }
